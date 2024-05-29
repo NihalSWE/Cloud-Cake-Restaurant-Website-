2158182a8c08853f0026f95fb57d47936453a447
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-05-29 16:41:55 +0600

    Adding UI on AdminPanel and customize the panel

---
+++ CloudCake/jazzmin.py
@@ -0,0 +1,50 @@
+JAZZMIN_SETTINGS = {
+    "site_title": "CloudCake Admin",
+    "site_header": "Cloud Cake & Foods",
+    "site_brand": "Cloud Cake & Foods",
+    "site_logo": "/assets/img/gallery/cc_icon.jpg",  
+    "login_logo": None,
+    "login_logo_dark": None,
+    "site_logo_classes": "navbar-brand-logo",  # Add CSS classes for site logo
+    "site_icon": None,
+    "welcome_sign": "Welcome to Cloud Cake & Foods",
+    "copyright": "Cloud Cake & Foods",
+    "search_model": ["auth.User", "auth.Group"],
+    "user_avatar": None,
+    "topmenu_links": [
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"model": "auth.User"},
+        {"app": "books"},
+    ],
+    "usermenu_links": [
+        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"model": "auth.user"}
+    ],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "hide_apps": [],
+    "hide_models": [],
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    "custom_links": {
+        "books": [{
+            "name": "Make Messages", 
+            "url": "make_messages", 
+            "icon": "fas fa-comments",
+            "permissions": ["books.view_book"]
+        }]
+    },
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+    },
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": False,
+    "custom_css":"/assets/css/admin.css",  # Add your custom CSS file here for further customization
+    "custom_js": None,
+    "use_google_fonts_cdn": True,
+    "show_ui_builder": False,
+    "changeform_format": "horizontal_tabs",
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+}

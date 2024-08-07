fe9431e239df0f755ff5fb17b8eecd6a4a36d612
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-08-07 12:52:47 +0600

    Update the url also , so that the website works perfectly

--- CloudCake/urls.py
+++ CloudCake/urls.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
-    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',include('products.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

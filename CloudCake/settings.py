e916bd5f8200d8baac25d3b5cd3f514181a0093b
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-08-07 12:33:39 +0600

    make some updation and set the function up so that the website run with DEBUG-False

--- CloudCake/settings.py
+++ CloudCake/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zj#b2#9uemr6az(5wf*7g7=dt!@q$i2-lze%#nu%o^ksfi!g26'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG: 
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR /'media'
 

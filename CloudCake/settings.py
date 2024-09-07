9490d9dbcff95d07eb894a8841231860a5f38fdf
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-09-07 10:52:06 +0600

    add cmpny logo on the donloaded  mrp chart

--- CloudCake/settings.py
+++ CloudCake/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # Adjust this as per your project structure
+# ]
+
 if DEBUG: 
    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
@@ -132,9 +137,7 @@ else:
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR /'media'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles'),  # Adjust this as per your project structure
-# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
